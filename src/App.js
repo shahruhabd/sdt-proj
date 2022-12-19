3368ff36ba967018226a5572c2f191a453d8ea3c
MaxLord18 <grekf2@mail.ru>
2022-12-20 01:20:19 +0600

    Fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/header/Header';
 import MainContainer from './components/main/Main-container'
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <MainContainer></MainContainer>
+        <Header></Header>
+        <MainContainer></MainContainer>
+        <Footer></Footer>
     </div>
   );
 }
